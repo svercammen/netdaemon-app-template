b0020c99e529e4ea52a64630da8b926f7e00881f
helto4real <tomas.hellstrom@yahoo.se>
2020-03-08 22:08:44 +0100

    New version of netdaemon, supporting namespaces

--- apps/HelloWorld/HelloWorld.cs
+++ apps/HelloWorld/HelloWorld.cs
@@ -1,10 +1,17 @@
 using System.Threading.Tasks;
 using JoySoftware.HomeAssistant.NetDaemon.Common;
-public class HelloWorldApp : NetDaemonApp
-{
-    public async override Task InitializeAsync()
-    {
-        Log("Hello World!");
 
+// Use unique namespaces for your apps if you going to share with others to avoid
+// conflicting names
+namespace HelloWorld
+{
+    public class HelloWorldApp : NetDaemonApp
+    {
+        public async override Task InitializeAsync()
+        {
+            Log("Hello World!");
+
+        }
     }
 }
+
